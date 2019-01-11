51ee017c8d80b69618578078b2b62d013df0a4d4
Hao Wu <gfphoenix78@gmail.com>
2019-01-11 02:57:13 +0000

    Update test cases
    1. remove prepare0, fini
    2. add test_insert_after_drop
    3. update keywords to upper case
    4. update result files for test_schema, test_extension, test_insert_after_drop

--- sql/prepare0.sql
+++
@@ -1,2 +0,0 @@
-create database diskquota;
-\! pg_ctl -D /tmp/pg_diskquota_test/data restart -l /dev/null 2>/dev/null >/dev/null
--- sql/test_insert_after_drop.sql
+++ sql/test_insert_after_drop.sql
@@ -1,20 +1,20 @@
-create database db_insert_after_drop;
+CREATE DATABASE db_insert_after_drop;
 \c db_insert_after_drop
-create extension diskquota;
+CREATE EXTENSION diskquota;
 -- Test Drop Extension
-create schema sdrtbl;
-select diskquota.set_schema_quota('sdrtbl', '1 MB');
-set search_path to sdrtbl;
-create table a(i int);
-insert into a select generate_series(1,100);
+CREATE SCHEMA sdrtbl;
+SELECT diskquota.set_schema_quota('sdrtbl', '1 MB');
+SET search_path TO sdrtbl;
+CREATE TABLE a(i int);
+INSERT INTO a SELECT generate_series(1,100);
 -- expect insert fail
-insert into a select generate_series(1,100000000);
-select pg_sleep(5);
-insert into a select generate_series(1,100);
-drop extension diskquota;
+INSERT INTO a SELECT generate_series(1,100000000);
+SELECT pg_sleep(5);
+INSERT INTO a SELECT generate_series(1,100);
+DROP EXTENSION diskquota;
 -- no sleep, it will take effect immediately
-insert into a select generate_series(1,100);
+INSERT INTO a SELECT generate_series(1,100);
 
-drop table a;
+DROP TABLE a;
 \c postgres
-drop database db_insert_after_drop;
+DROP DATABASE db_insert_after_drop;
