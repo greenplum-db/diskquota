908aaafcf3d7bac671900c8a6941552748f90ace
Xiaoran Wang <wxiaoran@vmware.com>
2021-07-28 14:44:53 +0800

    Fix one log message

--- quotamodel.c
+++ quotamodel.c
@@ -656,7 +656,7 @@ refresh_disk_quota_usage(bool is_init)
 	bool		pushed_active_snap = false;
 	bool		ret = true;
 
-	elog(WARNING, "refresh diskquota usage...");
+	elog(LOG, "refresh diskquota usage...");
 	StartTransactionCommand();
 
 	/*
