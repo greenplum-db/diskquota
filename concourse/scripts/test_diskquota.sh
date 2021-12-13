f355628256836a18c1ac6ac99048476fd7ceb712
Xing GUO <higuoxing@gmail.com>
2021-12-13 18:59:00 +0800

    Trying to make concourse happy.

--- concourse/scripts/test_diskquota.sh
+++ concourse/scripts/test_diskquota.sh
@@ -9,8 +9,23 @@ CUT_NUMBER=6
 
 source "${GPDB_CONCOURSE_DIR}/common.bash"
 source "${TOP_DIR}/diskquota_src/concourse/scripts/test_common.sh"
+
+function create_fake_gpdb_src() {
+	pushd gpdb_src
+	./configure --prefix=/usr/local/greenplum-db-devel \
+		    --with-perl --with-python --with-libxml \
+		    --without-zstd \
+		    --disable-orca --disable-gpcloud --enable-debug-extensions
+	popd
+
+	FAKE_GPDB_SRC=/tmp/build/"$(grep -rnw '/usr/local/greenplum-db-devel' -e 'abs_top_srcdir = .*' | head -n 1 | awk -F"/" '{print $(NF-1)}')"
+	mkdir -p ${FAKE_GPDB_SRC}
+	ln -s ${TOP_DIR}/gpdb_src ${FAKE_GPDB_SRC}/gpdb_src
+}
+
 function _main() {
 	time install_gpdb
+	create_fake_gpdb_src
 	time setup_gpadmin_user
 
 	time make_cluster
