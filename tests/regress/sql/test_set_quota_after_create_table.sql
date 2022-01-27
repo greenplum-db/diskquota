658f7e37117ff6f6c3b2b3c2f2903325cc4b152e
Xuebin Su <sxuebin@vmware.com>
2022-01-27 12:16:19 -0500

    Disable diskquota before CREATE TABLE

--- tests/regress/sql/test_set_quota_after_create_table.sql
+++ tests/regress/sql/test_set_quota_after_create_table.sql
@@ -1,15 +1,29 @@
+\! gpconfig -c shared_preload_libraries -v '' > /dev/null 
+\! gpstop -far > /dev/null
+\c
+
 CREATE ROLE test SUPERUSER;
 
 SET ROLE test;
 
+-- Create table "t" with diskquota disabled
 CREATE TABLE t (i) AS SELECT generate_series(1, 100000);
 
-SELECT diskquota.wait_for_worker_new_epoch();
+\! gpconfig -c shared_preload_libraries -v 'diskquota'> /dev/null 
+\! gpstop -far > /dev/null
+\c
+
+SET ROLE test;
+
+-- Init table_size to include table "t"
+SELECT diskquota.init_table_size_table();
 
 SELECT tableid::regclass, size FROM diskquota.table_size
 WHERE tableid = 't'::regclass AND segid = -1;
 
--- Ensure that t is not active
+SELECT 't'::regclass::oid;
+
+-- Ensure that table "t" is not active
 SELECT diskquota.diskquota_fetch_table_stat(0, ARRAY[]::oid[])
 FROM gp_dist_random('gp_id');
 
