620155551d063a965d8c83418bb1cb67e4a889d0
Chen Mulong <chenmulong@gmail.com>
2022-03-21 16:56:54 +0800

    Fix bash

--- cmake/regress_loop.sh
+++ cmake/regress_loop.sh
@@ -9,5 +9,5 @@ count=1
 while [ "$run_times" -lt 0 ] || [ "$count" -le "$run_times" ]; do
     echo "Run regress ${count} times"
     "${@:2}" || exit 1
-    count=$(( "$count" + 1 ))
+    count=$(( count + 1 ))
 done
