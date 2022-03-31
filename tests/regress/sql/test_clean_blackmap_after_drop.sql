7a7326c35a7e5672ff62eed6e623eb39771ca693
Sasasu <i@sasa.su>
2022-03-31 10:43:48 +0800

    clean blackmap after drop diskquota
    Fix hardlimit exceed after drop diskquota.
    
    fix this scenario:
      - create hard limit
      - blackmap on segment was updated
      - drop diskquota
      - the blackmap on segment is not cleanup
    
    use ObjectAccess hook on segment. when drop diskquota. clean up the in
    memory blackmap.

---
+++ tests/regress/sql/test_clean_blackmap_after_drop.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE test_clean_blackmap_after_drop;
+
+\c test_clean_blackmap_after_drop
+CREATE EXTENSION diskquota;
+
+\! gpconfig -c "diskquota.hard_limit" -v "on" > /dev/null
+\! gpstop -u > /dev/null
+
+CREATE ROLE r;
+SELECT diskquota.set_role_quota('r', '1MB');
+SELECT diskquota.wait_for_worker_new_epoch();
+
+CREATE TABLE b (t TEXT) DISTRIBUTED BY (t);
+ALTER TABLE b OWNER TO r;
+
+INSERT INTO b SELECT generate_series(1, 100000); -- fail
+
+DROP EXTENSION diskquota;
+
+INSERT INTO b SELECT generate_series(1, 100); -- ok
+
+\c contrib_regression
+DROP DATABASE test_clean_blackmap_after_drop;
+DROP ROLE r;
+
+\! gpconfig -c "diskquota.hard_limit" -v "off" > /dev/null
+\! gpstop -u > /dev/null
