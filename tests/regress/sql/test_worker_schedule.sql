d55a638f3b3c02aeb22f0f066d4051ddf25dfe00
Xiaoran Wang <wxiaoran@vmware.com>
2022-07-12 10:00:10 +0800

    Update test_worker_schedule

--- tests/regress/sql/test_worker_schedule.sql
+++ tests/regress/sql/test_worker_schedule.sql
@@ -7,6 +7,10 @@ CREATE DATABASE t1;
 CREATE DATABASE t2;
 CREATE DATABASE t3;
 CREATE DATABASE t4;
+CREATE DATABASE t5;
+CREATE DATABASE t6;
+CREATE DATABASE t7;
+CREATE DATABASE t8;
 --end_ignore
 
 \c t1
@@ -53,6 +57,39 @@ SELECT diskquota.wait_for_worker_new_epoch();
 SELECT diskquota.wait_for_worker_new_epoch();
 \c t4
 SELECT diskquota.wait_for_worker_new_epoch();
+
+\c t5
+CREATE EXTENSION diskquota;
+SELECT diskquota.wait_for_worker_new_epoch();
+
+\c t6
+CREATE EXTENSION diskquota;
+SELECT diskquota.wait_for_worker_new_epoch();
+
+\c t7
+CREATE EXTENSION diskquota;
+SELECT diskquota.wait_for_worker_new_epoch();
+
+\c t8
+CREATE EXTENSION diskquota;
+SELECT diskquota.wait_for_worker_new_epoch();
+
+\c t1
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t2
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t3
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t4
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t5
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t6
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t7
+SELECT diskquota.wait_for_worker_new_epoch();
+\c t8
+SELECT diskquota.wait_for_worker_new_epoch();
 --start_ignore
 \c t1
 DROP EXTENSION diskquota;
@@ -62,12 +99,26 @@ DROP EXTENSION diskquota;
 DROP EXTENSION diskquota;
 \c t4
 DROP EXTENSION diskquota;
+\c t5
+DROP EXTENSION diskquota;
+\c t6
+DROP EXTENSION diskquota;
+\c t7
+DROP EXTENSION diskquota;
+\c t8
+DROP EXTENSION diskquota;
 
 \c contrib_regression
 DROP DATABASE t1;
 DROP DATABASE t2;
 DROP DATABASE t3;
 DROP DATABASE t4;
+DROP DATABASE t5;
+DROP DATABASE t6;
+DROP DATABASE t7;
+DROP DATABASE t8;
 \! gpconfig -r diskquota.worker_timeout;
+\! gpconfig -r diskquota.naptime -v 1;
+\! gpconfig -r diskquota.max_workers;
 \! gpstop -u;
 --end_ignore
