e7f5014aa3b7f2f7e118b1e5cb0dca966593dfd2
zhrt123 <hzhang2@vmware.com>
2022-11-20 22:48:26 -0500

    Add dependency: cmocka.
    Use cmocka to do unit tests for `quota config`.

---
+++ test_unit_config_parse.c
@@ -0,0 +1,32 @@
+#include "c.h"
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cmocka.h>
+
+char *quota_type     = "\"quota_type\"";
+char *namespace_oid  = "\"namespace_oid\"";
+char *quota_limit_mb = "\"quota_limit_mb\"";
+
+static void
+test_parse(void **state)
+{
+	char expect_str[100];
+	sprintf(expect_str, "{%s: %d, %s: %d, %s: %d}", quota_type, 1, namespace_oid, 2, quota_limit_mb, 3);
+	printf("%s\n", expect_str);
+}
+
+int
+main(void)
+{
+	const struct CMUnitTest tests[] = {
+	        cmocka_unit_test(test_parse),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ test_unit_config_parse.h
@@ -0,0 +1,15 @@
+/* -------------------------------------------------------------------------
+ *
+ * relation_cache.h
+ *
+ * Copyright (c) 2020-Present VMware, Inc. or its affiliates
+ *
+ * IDENTIFICATION
+ *		diskquota/test_unit_config_parse.h
+ *
+ * -------------------------------------------------------------------------
+ */
+#ifndef TEST_UNIT_CONFIG_PARSE_H
+#define TEST_UNIT_CONFIG_PARSE_H
+
+#endif
