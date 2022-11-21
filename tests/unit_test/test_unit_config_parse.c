a2bff1547843c59dd9889670ae40988c0b616379
zhrt123 <hzhang2@vmware.com>
2022-11-21 05:53:10 -0500

    Move unit test file to /tests/unit_test.

--- tests/unit_test/test_unit_config_parse.c
+++ tests/unit_test/test_unit_config_parse.c
@@ -21,11 +21,21 @@ test_parse(void **state)
 	printf("%s\n", expect_str);
 }
 
+static void
+test_parse_fail(void **state)
+{
+	char expect_str[100];
+	sprintf(expect_str, "{%s: %d, %s: %d, %s: %d}", quota_type, 1, namespace_oid, 2, quota_limit_mb, 3);
+	printf("%s\n", expect_str);
+	assert_true(false);
+}
+
 int
 main(void)
 {
 	const struct CMUnitTest tests[] = {
 	        cmocka_unit_test(test_parse),
+	        // cmocka_unit_test(test_parse_fail),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
