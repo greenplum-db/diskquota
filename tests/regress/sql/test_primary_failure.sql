5403dfdb8ecc5e182dee38791092a639c0a892b9
Xiaoran Wang <wxiaoran@vmware.com>
2023-02-20 13:24:34 +0800

    Revert "Fix test_primary_failure test case"
    This reverts commit a597e84abed00569e63473e42e511367cc43ce2b.

--- tests/regress/sql/test_primary_failure.sql
+++ tests/regress/sql/test_primary_failure.sql
@@ -1,43 +1,31 @@
--- start_ignore
--- set the plpython_version
-show gp_server_version_num;
-\gset
-select CASE WHEN :gp_server_version_num < 70000 THEN 'plpythonu' else 'plpython3u' end plpython_version;
-\gset
--- end_ignore
-
 CREATE SCHEMA ftsr;
 SELECT diskquota.set_schema_quota('ftsr', '1 MB');
 SET search_path TO ftsr;
-create or replace language :plpython_version;
+create or replace language plpythonu;
 --
 -- pg_ctl:
 --   datadir: data directory of process to target with `pg_ctl`
 --   command: commands valid for `pg_ctl`
 --   command_mode: modes valid for `pg_ctl -m`  
 --
-create or replace function pg_ctl(gp_server_version int, datadir text, command text, command_mode text default 'immediate')
+create or replace function pg_ctl(datadir text, command text, command_mode text default 'immediate')
 returns text as $$
     import subprocess
     if command not in ('stop', 'restart'):
         return 'Invalid command input'
+
     cmd = 'pg_ctl -l postmaster.log -D %s ' % datadir
     cmd = cmd + '-W -m %s %s' % (command_mode, command)
-    if (gp_server_version < 70000):
-        return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace('.', '')
-    else:
-        return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, encoding='utf8').replace('.', '')
-$$ language :plpython_version;
 
-create or replace function pg_recoverseg(gp_server_version int, datadir text, command text)
+    return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace('.', '')
+$$ language plpythonu;
+
+create or replace function pg_recoverseg(datadir text, command text)
 returns text as $$
     import subprocess
     cmd = 'gprecoverseg -%s -d %s; exit 0; ' % (command, datadir)
-    if (gp_server_version < 70000):
-        return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace('.', '')
-    else:
-        return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, encoding='utf8').replace('.', '')
-$$ language :plpython_version;
+    return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace('.', '')
+$$ language plpythonu;
 
 CREATE TABLE a(i int) DISTRIBUTED BY (i);
 INSERT INTO a SELECT generate_series(1,100);
@@ -47,7 +35,7 @@ SELECT diskquota.wait_for_worker_new_epoch();
 INSERT INTO a SELECT generate_series(1,100);
 
 -- now one of primary is down
-select pg_ctl(:gp_server_version_num, (select datadir from gp_segment_configuration c where c.role='p' and c.content=0), 'stop');
+select pg_ctl((select datadir from gp_segment_configuration c where c.role='p' and c.content=0), 'stop');
 
 -- switch mirror to primary
 select gp_request_fts_probe_scan();
@@ -63,10 +51,10 @@ SELECT diskquota.set_schema_quota('ftsr', '200 MB');
 
 -- pull up failed primary
 -- start_ignore
-select pg_recoverseg(:gp_server_version_num, (select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'a');
-select pg_recoverseg(:gp_server_version_num, (select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'ar');
-select pg_recoverseg(:gp_server_version_num, (select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'a');
-select pg_recoverseg(:gp_server_version_num, (select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'ar');
+select pg_recoverseg((select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'a');
+select pg_recoverseg((select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'ar');
+select pg_recoverseg((select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'a');
+select pg_recoverseg((select datadir from gp_segment_configuration c where c.role='p' and c.content=-1), 'ar');
 -- check GPDB status
 select content, preferred_role, role, status, mode from gp_segment_configuration where content = 0;
 -- end_ignore
