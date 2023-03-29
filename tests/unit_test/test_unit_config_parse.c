4d07e7c13a0ba67a480c60fe29a042fbf8560294
zhrt123 <hzhang2@vmware.com>
2023-03-29 15:04:28 +0800

    Update config_parse.c

--- tests/unit_test/test_unit_config_parse.c
+++ tests/unit_test/test_unit_config_parse.c
@@ -1,8 +1,5 @@
 #include "c.h"
 
-#include "quota_config.h"
-#include "config_parse.h"
-
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -12,39 +9,25 @@
 
 #include <cmocka.h>
 
-#ifdef sprintf
-#undef sprintf
-#endif
-#ifdef printf
-#undef printf
-#endif
-
 char *quota_type     = "\"quota_type\"";
-char *db_oid         = "\"db_oid\"";
 char *namespace_oid  = "\"namespace_oid\"";
 char *quota_limit_mb = "\"quota_limit_mb\"";
 
 static void
 test_parse(void **state)
 {
-	char        expect_str[100];
-	QuotaConfig config;
+	char expect_str[100];
+	sprintf(expect_str, "{%s: %d, %s: %d, %s: %d}", quota_type, 1, namespace_oid, 2, quota_limit_mb, 3);
+	printf("%s\n", expect_str);
+}
 
-	memset(&config, 0, sizeof(config));
-	config.quota_type     = NAMESPACE_QUOTA;
-	config.keys[0]        = 123;
-	config.keys[1]        = 456;
-	config.quota_limit_mb = 100;
-
-	sprintf(expect_str, "{%s: %d, %s: %d, %s: %d, %s: %ld}", quota_type, NAMESPACE_QUOTA, db_oid, config.keys[0],
-	        namespace_oid, config.keys[1], quota_limit_mb, config.quota_limit_mb);
-
-	cJSON *ret         = do_construct_quota_config(&config);
-	cJSON *expect_json = cJSON_Parse(expect_str);
-
-	char *a = cJSON_Print(ret), *b = cJSON_Print(expect_json);
-	assert_memory_equal(a, b, strlen(a));
-	printf("%s\n%s\n", a, b);
+static void
+test_parse_fail(void **state)
+{
+	char expect_str[100];
+	sprintf(expect_str, "{%s: %d, %s: %d, %s: %d}", quota_type, 1, namespace_oid, 2, quota_limit_mb, 3);
+	printf("%s\n", expect_str);
+	assert_true(false);
 }
 
 int
@@ -52,6 +35,7 @@ main(void)
 {
 	const struct CMUnitTest tests[] = {
 	        cmocka_unit_test(test_parse),
+	        // cmocka_unit_test(test_parse_fail),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
