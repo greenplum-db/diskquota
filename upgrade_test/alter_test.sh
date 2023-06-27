5b21d048b6a867446359e8e66df98bf5169ef345
Chen Mulong <chenmulong@gmail.com>
2023-06-27 19:22:16 +0800

    Add alter extension upgrade test
    - By #340, diskquota should be able to upgraded directly from any previous
      version. A script is added to test this.
    - Modify the cmakefile so before installing/packaing, only previous so
      files will copied. This would help us to make patch release for
      2.0/2.1.

---
+++ upgrade_test/alter_test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Test if all the previous diskquota minor versions can be directly upgraded
+# to the current version.
+
+set -ex
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SRC_DIR="$(cd "$(dirname "${SCRIPT_PATH}")"/.. >/dev/null 2>&1 && pwd)"
+
+# Versions like major.minor
+CUR_VERSION=$(cut --delimiter="." --fields=1-2 "${SRC_DIR}/VERSION")
+ALL_VERSIONS=$(git tag | cut --delimiter="." --fields=1-2 | sort -V -u)
+VERSIONS_TO_TEST=()
+
+test_alter_from() {
+    local from_ver=$1
+    local to_ver=$CUR_VERSION
+
+    gpconfig -c shared_preload_libraries -v ""
+    gpstop -rai
+    dropdb diskquota --if-exists
+    dropdb diskquota_alter_test --if-exists
+    createdb diskquota
+
+    local so_name="diskquota"
+    if [ "${from_ver}" != "1.0" ];then
+        so_name="diskquota-${from_ver}"
+    fi
+
+    gpconfig -c shared_preload_libraries -v "${so_name}"
+    gpstop -rai
+
+    createdb diskquota_alter_test
+
+    # Test if the extension and be upgraded directly
+    psql -d diskquota_alter_test -c "CREATE EXTENSION diskquota version '${from_ver}'"
+    psql -d diskquota_alter_test -c "ALTER EXTENSION diskquota update to '${to_ver}'"
+    psql -d diskquota_alter_test -c "DROP EXTENSION diskquota"
+}
+
+# Find all minor versions before current one
+while IFS= read -r ver; do
+    if [ "${ver}" = "${CUR_VERSION}" ]; then
+        break
+    fi
+    if [ "${ver}" = "0.8" ]; then
+        continue
+    fi
+    VERSIONS_TO_TEST+=("${ver}")
+done <<< "$ALL_VERSIONS"
+
+for from_ver in "${VERSIONS_TO_TEST[@]}"; do
+    test_alter_from "${from_ver}"
+done
