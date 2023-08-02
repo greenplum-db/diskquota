ab11b5e91c0d45b737fc7497be53ff9ace7b0829
zhrt123 <hzhang2@vmware.com>
2023-08-02 11:58:55 +0800

    fix regress test.

--- tests/regress/sql/test_update_db_cache.sql
+++ tests/regress/sql/test_update_db_cache.sql
@@ -19,24 +19,7 @@ FROM diskquota.table_size
 WHERE tableid = 't'::regclass
 ORDER BY segid;
 
-DROP EXTENSION diskquota;
-
--- Create table without extension
-CREATE TABLE t_no_extension(i) AS SELECT generate_series(1, 100000)
-DISTRIBUTED BY (i);
-
-CREATE EXTENSION diskquota;
-
-SELECT diskquota_test.wait('SELECT diskquota_test.check_cur_db_status(''UNREADY'');');
-
--- Should find nothing since t_no_extension is not recorded.
-SELECT diskquota.diskquota_fetch_table_stat(0, ARRAY[]::oid[])
-FROM gp_dist_random('gp_id');
-
 DROP TABLE t;
-DROP TABLE t_no_extension;
-SELECT diskquota.pause();
-
 DROP EXTENSION diskquota;
 
 \c contrib_regression
