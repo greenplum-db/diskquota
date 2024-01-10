c1ed813690b354808e149148ad419faa88502609
Dennis Kovalenko <kds@arenadata.io>
2024-01-10 12:58:25 +0400

    Free tuple after use (#414)
    refresh_rejectmap looks for a tuple using SearchSysCacheCopy1 which retrieves
    a copy of the tuple allocating memory for it. However, refresh_rejectmap didn't
    free these tuple copies after use. If lots of oids were passed, diskquota could
    work incorrectly because of huge memory leak. This patch frees these tuples and
    prevents memory leaks.

--- src/quotamodel.c
+++ src/quotamodel.c
@@ -2054,6 +2054,8 @@ refresh_rejectmap(PG_FUNCTION_ARGS)
 								memcpy(&blocked_filenode_entry->auxblockinfo, &keyitem, sizeof(RejectMapEntry));
 								blocked_filenode_entry->segexceeded = rejectmapentry->segexceeded;
 							}
+
+							heap_freetuple(curr_tuple);
 						}
 					}
 					/*
@@ -2063,6 +2065,8 @@ refresh_rejectmap(PG_FUNCTION_ARGS)
 					break;
 				}
 			}
+
+			heap_freetuple(tuple);
 		}
 		else
 		{
